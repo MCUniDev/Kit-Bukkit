303f746e9e4ae153343f6d4cb6f83df956ce5b64
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-09-19 14:16:25 +0100

    Updated Kit command messages.

--- src/main/java/org/mcuni/kit/commands/CommandKit.java
+++ src/main/java/org/mcuni/kit/commands/CommandKit.java
@@ -10,12 +10,18 @@ public class CommandKit implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if (command.getName().equalsIgnoreCase("kit")) {
             if (args.length > 0) {
-                if (args[0].equals("modules")) {
-                    commandSender.sendMessage(ChatColor.GOLD + "Bounty, Broadcast, Config, Kit, Status");
+                if (args[0].equals("help")) {
+                    commandSender.sendMessage(ChatColor.GOLD + "[Kit] ------------ Kit Help ------------");
+                    commandSender.sendMessage(ChatColor.GOLD + "[Kit]" + ChatColor.YELLOW + "/kit - Main command");
+                    commandSender.sendMessage(ChatColor.GOLD + "[Kit]" + ChatColor.YELLOW + "/kit help - Show enabled commands");
+                    commandSender.sendMessage(ChatColor.GOLD + "[Kit]" + ChatColor.YELLOW + "/kit modules - Show enabled modules");
+                    commandSender.sendMessage(ChatColor.GOLD + "[Kit] ------------ Kit Help ------------");
+                } else if (args[0].equals("modules")) {
+                    commandSender.sendMessage(ChatColor.GOLD + "[Kit] Modules Enabled: Bounty, Broadcast, Config, Kit, Status");
                 }
             } else {
                 commandSender.sendMessage(ChatColor.GOLD + "[Kit] Running Kit version 1.0-SNAPSHOT for Minecraft 1.19.2");
-                commandSender.sendMessage(ChatColor.GOLD + "[Kit] Commands: /kit, /kit modules");
+                commandSender.sendMessage(ChatColor.GOLD + "[Kit] Use /kit help for more commands");
             }
         }
         return true;
