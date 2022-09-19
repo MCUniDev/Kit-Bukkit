b07ef515edd5ce83197bfc9ac2729817fd294e52
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-09-19 13:43:24 +0100

    Updated Kit command.

--- src/main/java/org/mcuni/kit/commands/CommandKit.java
+++ src/main/java/org/mcuni/kit/commands/CommandKit.java
@@ -4,19 +4,19 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.mcuni.kit.Broadcast;
 
 public class CommandKit implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
-        if (args.length > 0) {
-            if (args[0].equals("modules")) {
-                commandSender.sendMessage(ChatColor.GOLD + "Bounty, Broadcast, Config, Kit, Status");
+        if (command.getName().equalsIgnoreCase("kit")) {
+            if (args.length > 0) {
+                if (args[0].equals("modules")) {
+                    commandSender.sendMessage(ChatColor.GOLD + "Bounty, Broadcast, Config, Kit, Status");
+                }
+            } else {
+                commandSender.sendMessage(ChatColor.GOLD + "[Kit] Running Kit version 1.0-SNAPSHOT for Minecraft 1.19.2");
+                commandSender.sendMessage(ChatColor.GOLD + "[Kit] Commands: /kit, /kit modules");
             }
-        }
-        if(command.getName().equalsIgnoreCase("kit")) {
-            commandSender.sendMessage(ChatColor.GOLD + "[Kit] Running Kit version 1.0-SNAPSHOT for Minecraft 1.19.2");
-            commandSender.sendMessage(ChatColor.GOLD + "[Kit] Commands: /kit, /kit modules");
         }
         return true;
     }
