f4ea432db1c14650324d8c999d689346ddf27e99
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-12-07 19:17:27 +0000

    Updated status system.

--- src/main/java/org/mcuni/kit/Status.java
+++ src/main/java/org/mcuni/kit/Status.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static org.bukkit.Bukkit.getLogger;
 
@@ -25,6 +27,24 @@ public class Status {
         Bukkit.getLogger().info("[MCUni-Kit] Status module started.");
     }
 
+    public void statusTimer() {
+        Bukkit.getLogger().info("[MCUni-Kit][Status] Broadcast timer started.");
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("https://api.mcuni.org/v1/status/?network="+plugin.getConfig().getString("NetworkID")+"&server="+plugin.getConfig().getString("ServerID")+"&key="+plugin.getConfig().getString("APIKey")+"&status=2");
+                    new Scanner(url.openStream());
+                    getLogger().info("[MCUni-Kit][Status] Sent Status Ping to MCUni Network.");
+                } catch (IOException ex) {
+                    getLogger().severe("[MCUni-Kit][Status] Fatal error.");
+                    getLogger().severe(Arrays.toString(ex.getStackTrace()));
+                }
+            }
+        }, 30*60*1000, 5*60*1000);
+    }
+
     public void sendStatus(int Status) {
         try {
             URL url = new URL("https://kit.mcuni.org/api/"+plugin.APIVersion+"/status.php?network="+plugin.getConfig().getString("NetworkID")+"&server="+plugin.getConfig().getString("ServerID")+"&key="+plugin.getConfig().getString("APIKey")+"&status="+Status);
@@ -32,7 +52,7 @@ public class Status {
             getLogger().info("[MCUni-Kit][Status] Sent Status Ping to MCUni Network.");
         }
         catch(IOException ex) {
-            getLogger().severe("[Kit][Status] Fatal error.");
+            getLogger().severe("[MCUni-Kit][Status] Fatal error.");
             getLogger().severe(Arrays.toString(ex.getStackTrace()));
         }
         getLogger().info("[MCUni-Kit][Status][DEBUG] https://kit.mcuni.org/api/"+plugin.APIVersion+"/status.php?network="+plugin.getConfig().getString("NetworkID")+"&server="+plugin.getConfig().getString("ServerID")+"&key="+plugin.getConfig().getString("APIKey")+"&status="+Status);
