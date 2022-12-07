3982560536d7cf906961142de28cda10c188d773
lewmilburn <lewiscmilburn@gmail.com>
2022-12-07 21:25:17 +0000

    Updated Player Death system

--- src/main/java/org/mcuni/kit/events/PlayerDeath.java
+++ src/main/java/org/mcuni/kit/events/PlayerDeath.java
@@ -3,9 +3,13 @@ package org.mcuni.kit.events;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.mcuni.kit.Kit;
 
@@ -38,7 +42,25 @@ public class PlayerDeath implements Listener {
             Entity player = event.getEntity();
             String uuid = String.valueOf(player.getUniqueId());
             String location = String.valueOf(player.getLocation());
-            String cause = String.valueOf(player.getLastDamageCause());
+            EntityDamageEvent deathCause = player.getLastDamageCause();
+            String cause = "Unknown";
+            if (deathCause.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK ||
+                    deathCause.getCause() == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION ||
+                    deathCause.getCause() == EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK) {
+                Entity entity = (((EntityDamageByEntityEvent)deathCause).getDamager());
+                if(entity instanceof Player) {
+                    Player killerPlayer = (Player)entity;
+                    cause = "Murdered by " + killerPlayer.getName();
+                }
+                else {
+                    Monster killerMob = (Monster)entity;
+                    cause = killerMob.getType().toString();
+                    cause = cause.substring(0,1).toUpperCase() + cause.substring(1).toLowerCase();
+                }
+            } else {
+                cause = GetDeathReason(deathCause.getCause());
+            }
+            cause = cause.replace(" ", "%20");
             try {
                 if (plugin.getConfig().getBoolean("LogAPICalls")) {
                     getLogger().info("[Kit][PlayerDeath] Sending death data for player '" + player.getName() + "' with UUID '" + player.getUniqueId() + "'.");
@@ -73,5 +95,59 @@ public class PlayerDeath implements Listener {
             }
         }
         return "0";
+    }
+
+    private String GetDeathReason(EntityDamageEvent.DamageCause lastDamageCause) {
+        if (lastDamageCause == EntityDamageEvent.DamageCause.BLOCK_EXPLOSION) {
+            return "Exploded";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.LAVA) {
+            return "Lava damage";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.CONTACT) {
+            return "Touched a block with spikes";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.CRAMMING) {
+            return "Entity Cramming";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.DRAGON_BREATH) {
+            return "Dragon Breath";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.DROWNING) {
+            return "Drowned";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) {
+            return "Exploded";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.FALL) {
+            return "Fall damage";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.FALLING_BLOCK) {
+            return "Hit by a falling block";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.FIRE) {
+            return "Burnt by fire";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.FIRE_TICK) {
+            return "Burnt by fire";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.FLY_INTO_WALL) {
+            return "Flew into a wall";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.FREEZE) {
+            return "Froze to death";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.LIGHTNING) {
+            return "Struck by lightning";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.MAGIC) {
+            return "Potion or spell";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.POISON) {
+            return "Poison";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.PROJECTILE) {
+            return "Hit by a projectile";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.SONIC_BOOM) {
+            return "Warden's Sonic Boom";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.STARVATION) {
+            return "Starvation";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.SUFFOCATION) {
+            return "Suffocation";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.SUICIDE) {
+            return "Self inflicted";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.THORNS) {
+            return "Thorns enchantment";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.VOID) {
+            return "Fell through the world";
+        } else if (lastDamageCause == EntityDamageEvent.DamageCause.WITHER) {
+            return "Wither potion";
+        } else {
+            return "Unknown";
+        }
     }
 }
