cae452379b14169486ce003ea303f2636c61b451
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-12-07 19:26:26 +0000

    Added player death recording API

---
+++ src/main/java/org/mcuni/kit/events/PlayerDeath.java
@@ -0,0 +1,66 @@
+package org.mcuni.kit.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.mcuni.kit.Kit;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static org.bukkit.Bukkit.getLogger;
+
+public class PlayerDeath implements Listener {
+    public Kit plugin;
+
+    /**
+     * Constructor for the Player Death class.
+     * @param plugin References to the main kit plugin class.
+     */
+    public PlayerDeath(Kit plugin) {
+        this.plugin = plugin;
+        Bukkit.getLogger().info("[MCUni-Kit] Whitelist event handler started.");
+    }
+
+    /**
+     * EventHandler for when the player dies.
+     * @param event The event data.
+     */
+    @EventHandler(priority= EventPriority.HIGHEST)
+    public String onDeath(EntityDeathEvent event){
+        if (event.getEntityType() == EntityType.PLAYER) {
+            Entity player = event.getEntity();
+            String uuid = String.valueOf(player.getUniqueId());
+            String coordinates = String.valueOf(player.getLocation());
+            String world = String.valueOf(player.getWorld());
+            String cause = String.valueOf(player.getLastDamageCause());
+            try {
+                getLogger().info("[Kit][PlayerDeath] Sending death data for player '"+player.getName()+"' with UUID '"+player.getUniqueId()+"'.");
+                URL url = new URL("https://kit.mcuni.org/api/"+plugin.APIVersion+"/death.php?key="+plugin.getConfig().getString("APIKey")+"&network="+plugin.getConfig().getString("NetworkID")+"&server="+plugin.getConfig().getString("ServerID")+"&uuid="+uuid+"&coordinates="+coordinates+"&world="+world+"&cause="+cause);
+                Scanner s = new Scanner(url.openStream());
+                if (s.hasNextLine()) {
+                    String response = s.nextLine();
+                    if (response.equals("")) {
+                        getLogger().info("[Kit][PlayerDeath] There was no response from the server.");
+                        return "0";
+                    } else {
+                        getLogger().info("[Kit][PlayerDeath] Returned: " + response);
+                        return response;
+                    }
+                }
+            }
+            catch(IOException ex) {
+                getLogger().severe("[Kit][PlayerDeath] Fatal error.");
+                getLogger().severe(Arrays.toString(ex.getStackTrace()));
+            }
+            getLogger().info("[DEBUG] https://kit.mcuni.org/api/v3/death.php?key="+plugin.getConfig().getString("APIKey")+"&network="+plugin.getConfig().getString("NetworkID")+"&server="+plugin.getConfig().getString("ServerID")+"&uuid="+uuid+"&coordinates="+coordinates+"&world="+world+"&cause="+cause);
+        }
+        return "0";
+    }
+}
